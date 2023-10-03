3b5f5c0b7235bee8f454737324f2eac23eceb1d2
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-03 09:10:42 -0700

    operation 4

---
+++ javascript/operations4.js
@@ -0,0 +1,8 @@
+let valueinfo = "-3";
+let value = -valueinfo;
+console.log(value);//save value in vareable
+//********value store in third valeable********
+let go ="hello ";
+let go1 = "Mr Abubakar";
+let merge = go+go1;//merge 2 strings 
+console.log(merge); 
