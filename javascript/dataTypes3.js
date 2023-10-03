003e3f11e016f0ecc6e6bcd844183c04cf74dfa3
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-03 08:40:15 -0700

    adding data

---
+++ javascript/dataTypes3.js
@@ -0,0 +1,12 @@
+const Num = "Mr Ali";//string
+const Num1 = 22;//number
+const Num2 = true;//bolion
+ //converting data in 0 & 1 true convert 1 & false convert 0
+ console.table([Num,Num1,Num2]);
+// typeof is use to chack the type dataType 
+ const typeData = (typeof (Num));
+ console.log(typeData);
+ const typefata = (typeof (Num1));
+ console.log(typefata);
+ const typegata = (typeof (Num2));
+ console.log(typegata);
---
+++ javascript/variables2.js
@@ -0,0 +1,12 @@
+const accData = 144553
+let  accInfo = "mr.ali"
+var  accdate = "gojra"// it old variable not use in programming
+      info   = " ashraf colony gojra"
+
+ //accData = 145656// not allowd 
+ accInfo = "mr.ali"
+ //accdate = "gojrainfo"
+ info ="colony"
+
+ //console.log(accData)//it use for singe print 
+ console.table([accData,accInfo,accdate,info])//it is used for multi print console.table([])
