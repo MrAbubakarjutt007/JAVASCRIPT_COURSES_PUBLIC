17d9bf98ec6f19040b94faa9d663011a15294165
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-04 09:26:31 -0700

    adding primitivrdata and stakand heap

---
+++ javascript/primitivedataType6.js
@@ -0,0 +1,39 @@
+//we are  calling with value
+//7types
+//string,number,bolions,null(empty),undefined,symbol,BIgInt
+
+//***************************Non-primitiveDataType*********************** */
+
+//call by reference (Non-primitive)
+//Array,object,Function
+
+
+
+
+//const id= Symbol('1234');//Every symbol gave different result
+//const idData= Symbol('1234');
+//console.log(id===idData);
+//console.table([id,idData]);
+
+
+
+
+//array
+//const array= ["ali","abubakar","raza"];//array info
+
+
+
+
+//object
+// const object ={
+         
+//        name:" ali",
+//        age:22, 
+// }
+//function
+
+const fun=()=>{
+    console.log("Hello Mr Abubakar");
+} 
+ const data = fun();
+ console.log(data);
---
+++ javascript/stack_heap7.js
@@ -0,0 +1,15 @@
+//***********stack***********/
+
+let myData ="MR Abubakar";// it is use for primitive dataType 
+let myItem = myData;//it gives the copy of data
+console.log(myItem);
+//*********heap*********/
+let obj={
+   name:"ali",// it is use for non-primitive dataType
+   age:22,
+   email:"abubakar123@gmail.com",
+}
+console.log(obj);//it give the referance of data
+//***********output***********
+   //{ name: 'ali', age: 22, email: 'abubakar123@gmail.com' }
+    
