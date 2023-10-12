f3ce58775a88515f77fbb35e2440511c051c9521
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-12 09:09:49 -0700

    function data

---
+++ javascript/function17.js
@@ -0,0 +1,21 @@
+ function adding (n1, n2) {
+
+    let ans = n1*n2;
+     return ans
+}
+
+let result = adding(3,4);
+console.log(result);
+
+// 2nd
+
+let data=(user)=>{//user=umar default value 
+
+    if(!user){
+       console.log("plese enter value" )
+    }
+    return `${user} login`          
+}
+
+
+console.log(data("ali"));
