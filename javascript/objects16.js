09d147eb975d8bd153b7c88006afe99ffadb7bec
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-12 08:13:41 -0700

    object de_struting

---
+++ javascript/objects16.js
@@ -0,0 +1,12 @@
+//************OBJECT DE-STRUCRING***************/
+
+const de_structing = {
+     
+     compnyname: " Elo",
+     bestWorker: "Ali",
+     owner:"Abubakar jutt" 
+}
+//console.log(de_structing.owner) simple mathod
+
+const {owner:on} = de_structing;//De_structing mathod of object,owner:on it using rename owner
+console.log(on);// ...converting into on key
