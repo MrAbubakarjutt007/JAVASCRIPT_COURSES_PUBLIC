cbf62a913c095e5a62d06caa22ffe2ece2083829
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-13 03:13:18 -0700

    adding function

---
+++ javascript/function18.js
@@ -0,0 +1,38 @@
+//*****************function use with object*********/
+
+
+// let spradoperater =(...data)=>{
+//    return data
+// }
+
+
+// console.log(spradoperater("200","400","600"))
+
+// const obj={
+
+//    name:"ali",
+//    age:22
+// }
+
+
+
+// const MyFun=(Myobj)=>{
+   
+//     console.log(`My name is ${Myobj.name} and my is ${Myobj.age}`)
+//     return Myobj
+// }
+// MyFun(obj)
+
+const MyFun=(Myobj)=>{
+   
+    console.log(`My name is ${Myobj.name} and my age is ${Myobj.age}`)
+    return Myobj
+}
+MyFun({
+    
+    name:"raza",
+    age:23
+})
+
+
+
---
+++ javascript/function19.js
@@ -0,0 +1,11 @@
+// array use with function
+
+const ArrayInfo =[200, 300, 400, 500]
+
+const ArrayFun = (MyArray)=>{
+ 
+   console.log(`print data ${MyArray[3]}`)
+return MyArray[3]
+}
+
+ArrayFun(ArrayInfo)
