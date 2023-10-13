4f3eacd23dfe45e62e53d2e1b6badb86e73c7534
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-13 09:29:49 -0700

    IIFE code

---
+++ javascript/IIFE24.js
@@ -0,0 +1,10 @@
+//*********IIFE*******
+//function declared with name is called named IIfe
+(function IifeNameed(){
+      console.log(`my name is abubakar`);
+})();//two or more IIFE writing with;(qoma)
+
+//function declared with no name is called simple IIFE 
+((name)=>{
+   console.log(`i am a gantle${name}`)
+})(" man")
