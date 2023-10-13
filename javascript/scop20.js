7eda4ccee9219db84672a9148351212e73c17b5c
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-10-13 04:41:52 -0700

    function with in function with scop nesting

---
+++ javascript/scop20.js
@@ -0,0 +1,16 @@
+//***SCOP KARLLY BRACES{} USE WITH ANY CONDITION THEN HE CALLED SCOP********* */
+
+//if(){},while loop(){},function(){} in these condition karly braces{} are scops
+
+
+
+
+
+if(true){
+let a =23;
+const b= 20;
+var c =10;//not use able becouse he printing value out side the braces{}
+console.log(a);
+console.log(b); 
+}
+//console.log(c);
---
+++ javascript/scopnesting21.js
@@ -0,0 +1,24 @@
+//****FUNCTION WITH IN FUNCTION */
+function data(){
+             const name = "ali";
+
+             function value(){
+                  const agedata = 22;
+             
+                console.log(agedata)        
+             }
+             value()
+             console.log(name)
+}
+ data()
+ // IF STATEMENT WITH IN IF
+
+if(true){
+       const datinfo = "i am very gantle man";
+       if(datinfo ==="i am very gantle man"){
+              const info = "becouse i am a web developer"
+             console.log(info)                          
+       } 
+       console.log(datinfo)
+}
+
